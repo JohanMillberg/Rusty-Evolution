405bd6c1a568ac71e5daea34d367339da71a50c8
JohanMillberg <johanmillberg@me.com>
2025-02-05 00:39:09 +0100

    initial commit

---
+++ target/debug/deps/lib_neural_network-58a7bf1d0571bd0e.d
@@ -0,0 +1,5 @@
+/home/johan/code_files/rust_projects/rusty_evolution/target/debug/deps/liblib_neural_network-58a7bf1d0571bd0e.rmeta: libs/neural-network/src/lib.rs
+
+/home/johan/code_files/rust_projects/rusty_evolution/target/debug/deps/lib_neural_network-58a7bf1d0571bd0e.d: libs/neural-network/src/lib.rs
+
+libs/neural-network/src/lib.rs:
