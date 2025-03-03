530cfde55c789a00a67741a49aa9433af5e46c44
JohanMillberg <johanmillberg@me.com>
2025-03-03 21:39:05 +0100

    added genetic algorithm test, created wasm lib

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -24,6 +24,7 @@ impl Chromosome {
 pub trait Individual {
     fn fitness(&self) -> f32;
     fn chromosome(&self) -> &Chromosome;
+    fn create(chromosome: Chromosome) -> Self;
 }
 
 pub trait MutationMethod {
@@ -130,16 +131,22 @@ impl SelectionMethod for RouletteWheelSelection {
 pub struct GeneticAlgorithm<S> {
     selection_method: S,
     crossover_method: Box<dyn CrossoverMethod>,
+    mutation_method: Box<dyn MutationMethod>,
 }
 
 impl<S> GeneticAlgorithm<S>
 where
     S: SelectionMethod,
 {
-    pub fn new(selection_method: S, crossover_method: impl CrossoverMethod + 'static) -> Self {
+    pub fn new(
+        selection_method: S,
+        crossover_method: impl CrossoverMethod + 'static,
+        mutation_method: impl MutationMethod + 'static,
+    ) -> Self {
         Self {
             selection_method,
             crossover_method: Box::new(crossover_method),
+            mutation_method: Box::new(mutation_method),
         }
     }
 
@@ -153,7 +160,10 @@ where
                 let parent_a = self.selection_method.select(rng, population).chromosome();
                 let parent_b = self.selection_method.select(rng, population).chromosome();
                 let mut child = self.crossover_method.crossover(rng, parent_a, parent_b);
-                todo!("Code mutation")
+
+                self.mutation_method.mutate(rng, &mut child);
+
+                I::create(child)
             })
             .collect()
     }
@@ -167,23 +177,43 @@ mod tests {
     use std::collections::BTreeMap;
     use std::iter::FromIterator;
 
-    #[derive(Clone, Debug)]
-    struct TestIndividual {
-        fitness: f32,
+    #[derive(Clone, Debug, PartialEq)]
+    pub enum TestIndividual {
+        WithChromosome { chromosome: Chromosome },
+        WithFitness { fitness: f32 },
+    }
+
+    impl PartialEq for Chromosome {
+        fn eq(&self, other: &Self) -> bool {
+            approx::relative_eq!(self.genes.as_slice(), other.genes.as_slice())
+        }
     }
 
     impl TestIndividual {
         fn new(fitness: f32) -> Self {
-            Self { fitness }
+            Self::WithFitness { fitness }
         }
     }
 
     impl Individual for TestIndividual {
-        fn fitness(&self) -> f32 {
-            self.fitness
+        fn create(chromosome: Chromosome) -> Self {
+            Self::WithChromosome { chromosome }
         }
+
         fn chromosome(&self) -> &Chromosome {
-            panic!("not implemented for test individuals")
+            match self {
+                Self::WithChromosome { chromosome } => chromosome,
+                Self::WithFitness { .. } => {
+                    panic!("not supported for TestIndividual::WithFitness")
+                }
+            }
+        }
+
+        fn fitness(&self) -> f32 {
+            match self {
+                Self::WithFitness { fitness } => *fitness,
+                Self::WithChromosome { chromosome } => chromosome.iter().sum(),
+            }
         }
     }
 
@@ -225,7 +255,42 @@ mod tests {
         assert_eq!(actual_histogram, expected_histogram)
     }
 
-    mod gausssian_mutation {
+    #[test]
+    fn genetic_algorithm() {
+        fn individual(genes: &[f32]) -> TestIndividual {
+            TestIndividual::create(genes.iter().cloned().collect())
+        }
+
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let ga = GeneticAlgorithm::new(
+            RouletteWheelSelection,
+            UniformCrossover,
+            GaussianMutation::new(0.5, 0.5),
+        );
+
+        let mut population = vec![
+            individual(&[0.0, 0.0, 0.0]),
+            individual(&[1.0, 1.0, 1.0]),
+            individual(&[1.0, 2.0, 1.0]),
+            individual(&[1.0, 2.0, 4.0]),
+        ];
+
+        for _ in 0..10 {
+            population = ga.evolve(&mut rng, &population);
+        }
+
+        let expected_population = vec![
+            individual(&[0.4476949, 2.0648358, 4.3058133]),
+            individual(&[1.2126867, 1.5538777, 2.886911]),
+            individual(&[1.0617678, 2.265739, 4.428764]),
+            individual(&[0.95909685, 2.4618788, 4.024733]),
+        ];
+
+        assert_eq!(population, expected_population);
+    }
+
+    mod gaussian_mutation {
         use super::*;
 
         fn actual(chance: f32, coeff: f32) -> Vec<f32> {
